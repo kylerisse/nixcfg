a2903b02f02b62ed31c7af8456f16f184dc59b58
kylerisse <kylerisse@users.noreply.github.com>
2023-03-12 19:10:53 -0700

    adds ssh server module

---
+++ modules/ssh-server/default.nix
@@ -0,0 +1,9 @@
+{ config, pkgs, ... }:
+{
+  services.openssh = {
+    enable = true;
+    passwordAuthentication = false;
+    kbdInteractiveAuthentication = false;
+    permitRootLogin = "no";
+  };
+}
