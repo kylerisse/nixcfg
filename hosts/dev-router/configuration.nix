72786200bcc94d074962583484ccf4aebf18c8bd
kylerisse <kylerisse@users.noreply.github.com>
2023-03-21 21:06:40 -0700

    parameterizes dhcp-server config options

--- hosts/dev-router/configuration.nix
+++ hosts/dev-router/configuration.nix
@@ -16,6 +16,24 @@
   dualhome-nat.externalInterface = "enp1s0";
   dualhome-nat.internalCIDR = "192.168.70.0/24";
 
+  dhcp-server.interfaces = [ "enp2s0" ];
+  dhcp-server.dns = "192.168.73.1";
+  dhcp-server.domain = "risse.tv";
+  dhcp-server.reservations = [
+    { hostname = "temp"; hw-address = "52:54:00:59:ca:fb"; ip-address = "192.168.70.92"; }
+  ];
+  dhcp-server.v4subnets = [{
+    subnet = "192.168.70.0/24";
+    id = 19216870;
+    user-context.vlan = "kvm-unrouted";
+    pools = [
+      { pool = "192.168.70.100 - 192.168.70.199"; }
+    ];
+    option-data = [
+      { name = "routers"; data = "192.168.70.1"; }
+    ];
+  }];
+
   networking = {
     hostName = "dev-router";
     interfaces = {
--- modules/dhcp-server/default.nix
+++ modules/dhcp-server/default.nix
@@ -1,13 +1,95 @@
 { config, pkgs, inputs, lib, ... }:
+let
+    format = pkgs.formats.json {};
+    cfg = config.dhcp-server;
+in
 {
-    environment.systemPackages= with pkgs; [
-        kea
-    ];
-    # TODO: this file assume dev-router, how to paramaterize?
-    services.kea = {
-        dhcp4 = {
+    options.dhcp-server = {
+        interfaces = lib.mkOption {
+            type = lib.types.listOf lib.types.str;
+            default = [ "eth0" ];
+            description = "DHCP listen interface";
+        };
+        dns = lib.mkOption {
+            type = lib.types.str;
+            default = "1.1.1.1,8.8.8.8";
+            description = "List of DNS servers";
+        };
+        domain = lib.mkOption {
+            type = lib.types.str;
+            default = "example.com";
+            description = "Domain name";
+        };
+        reservations = lib.mkOption {
+            type = format.type;
+            default = [
+                {
+                    hostname = "example1";
+                    hw-address = "ab:cd:ef:12:34:56";
+                    ip-address = "192.168.0.5";
+                }
+                {
+                    hostname = "example2";
+                    hw-address = "90:ef:12:34:56:78";
+                    ip-address = "192.168.0.6";
+                }
+            ];
+        };
+        v4subnets = lib.mkOption {
+            type = format.type;
+            default = [{
+                subnet = "192.168.0.0/24";
+                id = 12345;
+                user-context = {
+                    vlan = "default";
+                };
+                pools = [{
+                    pool = "192.168.0.10 - 192.168.0.254";
+                }];
+                option-data = [{
+                    name = "routers";
+                    data = "192.168.0.1";
+                }];
+			}];
+            description = "ipv4 subnet config";
+        };
+    };
+    config = {
+        environment.systemPackages= with pkgs; [
+            kea
+        ];
+        services.kea.dhcp4 = {
             enable = true;
-            configFile = ./kea.json;
+            settings = {
+                loggers = [{
+                    name = "*";
+                    severity = "DEBUG";
+                }];
+                valid-lifetime = 86400;
+                renew-timer = 21600;
+                rebind-timer = 43200;
+                interfaces-config = {
+                    interfaces = cfg.interfaces;
+                };
+                lease-database = {
+                    type = "memfile";
+                    persist = true;
+                    name = "/var/lib/kea/dhcp4.leases";
+                };
+                option-data = [
+                    {
+                        name = "domain-name-servers";
+                        data = cfg.dns;
+                    }
+                    {
+                        name = "domain-name";
+                        data = cfg.domain;
+                    }
+                ];
+                reservation-mode = "global";
+                reservations = cfg.reservations;
+                subnet4 = cfg.v4subnets;
+            };
         };
     };
 }
