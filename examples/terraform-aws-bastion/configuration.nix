e500b3c6b8968c769323627322e34dbaafbf6a9f
kylerisse <kylerisse@users.noreply.github.com>
2023-04-21 18:56:39 -0700

    removes unneeded ssh key

--- examples/terraform-aws-bastion/configuration.nix
+++ examples/terraform-aws-bastion/configuration.nix
@@ -18,7 +18,6 @@
 
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPirA5WUlTLXEol/yr+QJDeWa3S8GW0u4TXzSxBxRrbs"
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJaBu1HK/Ou9wV2Ebz7NKVsRSZEKiJfvwmcictvnu8lu"
     ];
   };
 
--- modules/users/default.nix
+++ modules/users/default.nix
@@ -6,7 +6,6 @@
     extraGroups = [ "wheel" ];
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPirA5WUlTLXEol/yr+QJDeWa3S8GW0u4TXzSxBxRrbs"
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJaBu1HK/Ou9wV2Ebz7NKVsRSZEKiJfvwmcictvnu8lu"
     ];
   };
   security.sudo.wheelNeedsPassword = false;
