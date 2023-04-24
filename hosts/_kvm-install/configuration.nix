1aae1c708726242f60eb5d523537cd55b596e67a
kylerisse <kylerisse@users.noreply.github.com>
2023-04-24 14:52:43 -0700

    adds new ssh key

--- hosts/_kvm-install/configuration.nix
+++ hosts/_kvm-install/configuration.nix
@@ -26,6 +26,7 @@
     extraGroups = [ "wheel" ];
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPirA5WUlTLXEol/yr+QJDeWa3S8GW0u4TXzSxBxRrbs"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILd1LH8ULHcy7jk0GtajE2N5EIjzoytcgylAYc6CzR6+"
     ];
   };
 
