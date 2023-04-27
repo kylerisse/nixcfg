1dc6f6b4407eb699804b3360024b919a0a3572c4
kylerisse <kylerisse@users.noreply.github.com>
2023-04-26 19:01:42 -0700

    add zugzug ssh key

--- examples/terraform-aws-bastion/configuration.nix
+++ examples/terraform-aws-bastion/configuration.nix
@@ -19,6 +19,7 @@
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPirA5WUlTLXEol/yr+QJDeWa3S8GW0u4TXzSxBxRrbs"
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILd1LH8ULHcy7jk0GtajE2N5EIjzoytcgylAYc6CzR6+"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMlEPbMnefiPfCTKb9lOzPzfnOVAohO08myWWMm9EJxZ"
     ];
   };
 
