93eb8f0ff09da24266569dba824971d90b36e0ad
kylerisse <kylerisse@users.noreply.github.com>
2023-04-28 09:49:35 -0700

    cleanup comments in zugzug configuration

--- darwin/zugzug/darwin-configuration.nix
+++ darwin/zugzug/darwin-configuration.nix
@@ -135,16 +135,11 @@ in
     # End Nix
   '';
 
-  # Use a custom configuration.nix location.
-  # $ darwin-rebuild switch -I darwin-config=$HOME/.config/nixpkgs/darwin/configuration.nix
-  # environment.darwinConfig = "$HOME/.config/nixpkgs/darwin/configuration.nix";
-
-  # Auto upgrade nix package and the daemon service.
   services.nix-daemon.enable = true;
   nix.package = pkgs.nix;
 
   # Create /etc/zshrc that loads the nix-darwin environment.
-  programs.zsh.enable = true; # default shell on catalina
+  programs.zsh.enable = true; # default shell on ventura
 
   # Used for backwards compatibility, please read the changelog before changing.
   # $ darwin-rebuild changelog
