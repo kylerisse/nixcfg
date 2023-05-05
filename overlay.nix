4be59f61717f6c04ae6d7176e569933a37c0dea2
kylerisse <kylerisse@users.noreply.github.com>
2023-05-05 08:49:03 -0700

    add terraform 1.4.6 nix package for arm darwin and x86 linux

--- overlay.nix
+++ overlay.nix
@@ -2,4 +2,5 @@ self: super:
 {
   terraform_1-3-9 = super.callPackage ./pkgs/terraform_1-3-9 { };
   terraform_1-4-2 = super.callPackage ./pkgs/terraform_1-4-2 { };
+  terraform_1-4-6 = super.callPackage ./pkgs/terraform_1-4-6 { };
 }
