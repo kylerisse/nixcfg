774cd681cf4873a428d927fee54c7646b0e8c8df
kylerisse <kylerisse@users.noreply.github.com>
2024-03-21 14:56:47 -0700

    update ssh-server module settings names

--- modules/ssh-server/default.nix
+++ modules/ssh-server/default.nix
@@ -3,9 +3,9 @@
   services.openssh = {
     enable = true;
     settings = {
-      passwordAuthentication = false;
-      kbdInteractiveAuthentication = false;
-      permitRootLogin = "no";
+      PasswordAuthentication = false;
+      KbdInteractiveAuthentication = false;
+      PermitRootLogin = "no";
     };
   };
   networking.firewall.allowedTCPPorts = [ 22 ];
