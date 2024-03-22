b49f7882ea0f4f4fb919cb7fce78f05eba564f1a
kylerisse <kylerisse@users.noreply.github.com>
2024-03-21 21:12:17 -0700

    add config option for forwarders to dns-server module

--- modules/dns-server/default.nix
+++ modules/dns-server/default.nix
@@ -16,6 +16,11 @@ in
       description = "CIDRs allowed to recursive query";
       default = [ "192.168.0.0/24" "127.0.0.1/32" ];
     };
+    forwarders = lib.mkOption {
+      type = lib.types.listOf lib.types.str;
+      default = [ "1.1.1.1" "9.9.9.9" ];
+      description = "DNS forwarders";
+    };
     zones = lib.mkOption {
       type = format.type;
       default = {
@@ -45,8 +50,8 @@ in
   config = {
     services.bind.enable = cfg.enable;
     services.bind.cacheNetworks = cfg.allowedCIDRs;
-    services.bind.forwarders = [ "1.1.1.1" "9.9.9.9" ];
-    services.bind.forward = "first";
+    services.bind.forwarders = cfg.forwarders;
+    services.bind.forward = "only";
     services.bind.listenOn = cfg.listenOn;
     services.bind.zones = cfg.zones;
     networking.firewall.allowedTCPPorts = [ 53 ];
