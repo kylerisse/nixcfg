644e212cd5a281a64022e53de8a914459f89967f
kylerisse <kylerisse@users.noreply.github.com>
2024-03-28 09:46:05 -0700

    derp wip

--- modules/users/default.nix
+++ modules/users/default.nix
@@ -3,6 +3,7 @@
   users.users.kylerisse = {
     isNormalUser = true;
     uid = 9001;
+    description = "kylerisse";
     extraGroups = [ "wheel" ];
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPirA5WUlTLXEol/yr+QJDeWa3S8GW0u4TXzSxBxRrbs"
