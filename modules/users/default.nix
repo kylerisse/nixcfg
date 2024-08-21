523dc862bc1fc03f6b4c58ea95b990f35f20614b
kylerisse <kylerisse@users.noreply.github.com>
2024-08-20 23:13:02 -0700

    update keys

--- modules/users/default.nix
+++ modules/users/default.nix
@@ -6,9 +6,8 @@
     description = "ls";
     extraGroups = [ "wheel" ];
     openssh.authorizedKeys.keys = [
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPirA5WUlTLXEol/yr+QJDeWa3S8GW0u4TXzSxBxRrbs"
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILd1LH8ULHcy7jk0GtajE2N5EIjzoytcgylAYc6CzR6+"
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICsFjPrXwIcG1uJER9JTIVQVfiBMrXqDfmnFKZJG8bCm"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPcTYYr/TGH4vRCaY4WU4Qc7RlzzBOHv2XYxGwCzV+fg p"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKX8NM1OQECwhNTQE0qAm422uq9L0i0Y/hvPPc4tHIOX a"
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMlEPbMnefiPfCTKb9lOzPzfnOVAohO08myWWMm9EJxZ"
     ];
   };
