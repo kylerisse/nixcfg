e3c2f38885152aff1a4320b10becaab2d839b56d
kylerisse <kylerisse@users.noreply.github.com>
2024-08-21 19:42:32 -0700

    add nix-common module

---
+++ modules/nix-common/default.nix
@@ -0,0 +1,30 @@
+{ config, lib, pkgs, ... }:
+let
+  cfg = config.nix-common;
+in
+{
+  options.nix-common = {
+    enable = lib.mkEnableOption (lib.mdDoc "Baseline Nix and Nixpkgs settings");
+    isDarwin = lib.mkEnableOption (lib.mdDoc "Darwin specific nix/nixpkg settings");
+  };
+  config = lib.mkIf cfg.enable {
+    nix = {
+      package = pkgs.nixVersions.latest;
+      gc = {
+        automatic = true;
+      };
+      settings = {
+        trusted-users = if cfg.isDarwin then [ "@admin" ] else [ "@wheel" ];
+        auto-optimise-store = !cfg.isDarwin; # don't optimize on Darwin, do on NixOs
+        experimental-features = [
+          "nix-command"
+          "flakes"
+        ];
+        keep-derivations = true;
+        keep-outputs = true;
+      };
+    };
+    nixpkgs.config.allowUnfree = true;
+    system.stateVersion = if cfg.isDarwin then 4 else config.system.nixos.version;
+  };
+}
