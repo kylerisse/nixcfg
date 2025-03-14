fe66344d51349e4331cf80a11534afd7a7d6f4c3
kylerisse <kylerisse@users.noreply.github.com>
2025-03-13 21:35:36 -0700

    qube: remove as k8s master

--- machines/qube/configuration.nix
+++ machines/qube/configuration.nix
@@ -3,21 +3,6 @@
   nix-common.enable = true;
   ssh-server.enable = true;
 
-  kube-cluster = {
-    enable = true;
-    isMaster = true;
-    masterIP = "192.168.73.4";
-    masterHostname = "qube";
-    masterPort = 6443;
-  };
-
-  networking.extraHosts =
-    ''
-      192.168.73.4 qube
-      192.168.73.2 pi3
-      192.168.73.3 pi4
-    '';
-
   imports =
     [
       (modulesPath + "/installer/scan/not-detected.nix")
