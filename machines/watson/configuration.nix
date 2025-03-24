183a0361ee8490fbf99db2be5e82ff26fce9ac32
kylerisse <kylerisse@users.noreply.github.com>
2025-03-23 20:01:37 -0700

    watson: set ollama and open-webui to stable versions

--- machines/watson/configuration.nix
+++ machines/watson/configuration.nix
@@ -231,7 +231,6 @@ in
 
   services.ollama = {
     enable = true;
-    package = pkgs-unstable.ollama;
     loadModels = [
       "llama3.3:latest"
       "gemma2:latest"
@@ -243,7 +242,6 @@ in
   };
   services.open-webui = {
     enable = true;
-    package = pkgs-unstable.open-webui;
     environment = {
       ANONYMIZED_TELEMETRY = "False";
       DO_NOT_TRACK = "True";
