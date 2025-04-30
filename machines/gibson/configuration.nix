6251130aacc495a7f0f0fe01ed83b22aa676bb72
kylerisse <kylerisse@users.noreply.github.com>
2025-04-29 22:54:46 -0700

    gibson: fail2ban add nginx probe jail

--- machines/gibson/configuration.nix
+++ machines/gibson/configuration.nix
@@ -21,6 +21,14 @@
   ssh-server.enable = true;
   nix-common.enable = true;
 
+  environment.etc = {
+    # taken from https://nixos.wiki/wiki/Fail2ban
+    # Defines a filter that detects URL probing by reading the Nginx access log
+    "fail2ban/filter.d/nginx-url-probe.local".text = pkgs.lib.mkDefault (pkgs.lib.mkAfter ''
+      [Definition]
+      failregex = ^<HOST>.*(GET /(wp-|admin|boaform|phpmyadmin|\.env|\.git)|\.(dll|so|cfm|asp)|(\?|&)(=PHPB8B5F2A0-3C92-11d3-A3A9-4C7B08C10000|=PHPE9568F36-D428-11d2-A769-00AA001ACF42|=PHPE9568F35-D428-11d2-A769-00AA001ACF42|=PHPE9568F34-D428-11d2-A769-00AA001ACF42)|\\x[0-9a-zA-Z]{2})
+    '');
+  };
   services.fail2ban = {
     enable = true;
     maxretry = 2;
@@ -29,6 +37,15 @@
       enable = true;
       multipliers = "1 2 4 8 16 32 64";
       overalljails = true;
+    };
+    jails = {
+      "nginx-url-probe".settings = {
+        enabled = true;
+        filter = "nginx-url-probe";
+        logpath = "/var/log/nginx/access.log";
+        backend = "auto";
+        action = "%(action_)s[blocktype=DROP]";
+      };
     };
   };
 
