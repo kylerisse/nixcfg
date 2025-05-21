1363bfab303f2a7db0423e73c9da7682ba3cf961
kylerisse <kylerisse@users.noreply.github.com>
2025-05-21 10:25:45 -0700

    inputs: add nixos-25.05

--- flake.nix
+++ flake.nix
@@ -4,6 +4,7 @@
   inputs = {
     # linux
     nixos-2411.url = "github:nixos/nixpkgs/nixos-24.11?shallow=1";
+    nixos-2505.url = "github:nixos/nixpkgs/nixos-25.05?shallow=1";
     nixos-unstable.url = "github:nixos/nixpkgs/nixos-unstable?shallow=1";
     nixos-hardware.url = "github:nixos/nixos-hardware?shallow=1";
     # mac
@@ -19,6 +20,7 @@
   outputs =
     inputs@{ self
     , nixos-2411
+    , nixos-2505
     , nixos-unstable
     , nixos-hardware
     , nixpkgs-darwin
