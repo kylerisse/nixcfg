ff21169fcaf806156003d88737545f6d55780bae
kylerisse <kylerisse@users.noreply.github.com>
2025-12-30 13:50:44 -0800

    watson: switch to nvidia driver

--- machines/watson/hardware-configuration.nix
+++ machines/watson/hardware-configuration.nix
@@ -62,4 +62,8 @@
     enable = true;
     enable32Bit = true;
   };
+
+  services.xserver.videoDrivers = [ "nvidia" ];
+  hardware.nvidia.open = false;
+  hardware.nvidia.modesetting.enable = true;
 }
