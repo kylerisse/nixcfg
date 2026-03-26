aa6b5f0a2dd0c262df54b87c5f58c14bfa7ad858
kylerisse <kylerisse@users.noreply.github.com>
2026-03-25 20:12:17 -0700

    flake: simplify configs with mkSystem func

--- flake.nix
+++ flake.nix
@@ -41,6 +41,61 @@
       pkgsFor = system: import (if system == "aarch64-darwin" then nixpkgs-darwin else nixos-unstable) {
         inherit system;
       };
+      mkSystem =
+        { nixpkgs ? nixos-2511
+        , system ? "x86_64-linux"
+        , modules
+        , extraSpecialArgs ? { }
+        ,
+        }:
+        nixpkgs.lib.nixosSystem {
+          inherit system modules;
+          specialArgs = { inherit nixpkgs inputs; } // extraSpecialArgs;
+        };
+      all =
+        ({ modulePath, ... }: {
+          imports = [
+            ./modules/nix-common
+            ./modules/ssh-server
+            ./modules/scale-signs
+            ./modules/kube-cluster
+            ./modules/mrtg
+            ./modules/scale-simulator
+            ./modules/wasgeht
+          ];
+        });
+      users =
+        ({ modulePath, ... }: {
+          imports = [
+            ./modules/users
+          ];
+        });
+      kvm-guest =
+        ({ modulePath, ... }: {
+          imports = [
+            ./modules/kvm-guest
+          ];
+        });
+      soho-router =
+        ({ modulePath, ... }: {
+          imports = [
+            ./modules/dualhome-nat
+            ./modules/dhcp-server
+            ./modules/dns-server
+          ];
+        });
+      images = {
+        doImage = mkSystem {
+          modules = [ all users ./imgs/do.nix ];
+        };
+        installerImage = mkSystem {
+          modules = [ all ./imgs/gnome-installer.nix ];
+        };
+        piImage = mkSystem {
+          system = "aarch64-linux";
+          modules = [ all users ./imgs/pi.nix ];
+        };
+      };
     in
     {
       devShells = forAllSystems (system:
@@ -84,13 +139,13 @@
           };
         in
         {
-          pi3Image = (self._images.piImage.extendModules {
+          pi3Image = (images.piImage.extendModules {
             modules = [
               "${nixos-2511}/nixos/modules/installer/sd-card/sd-image-aarch64.nix"
               nixos-hardware.nixosModules.raspberry-pi-3
             ];
           }).config.system.build.sdImage;
-          pi4Image = (self._images.piImage.extendModules {
+          pi4Image = (images.piImage.extendModules {
             modules = [
               "${nixos-2511}/nixos/modules/installer/sd-card/sd-image-aarch64.nix"
               nixos-hardware.nixosModules.raspberry-pi-4
@@ -110,8 +165,8 @@
           openwrt-archer-a7-v5 = pkgs.callPackage ./pkgs/openwrt-archer-a7-v5 { };
           openwrt-archer-c7-v2 = pkgs.callPackage ./pkgs/openwrt-archer-c7-v2 { };
           openwrt-one = pkgs.callPackage ./pkgs/openwrt-one { };
-          doImage = self._images.doImage.config.system.build.digitalOceanImage;
-          installerISO = self._images.installerImage.config.system.build.isoImage;
+          doImage = images.doImage.config.system.build.digitalOceanImage;
+          installerISO = images.installerImage.config.system.build.isoImage;
           docket-unstable = pkgs.callPackage ./pkgs/docket-unstable { };
           wasgeht = pkgs.callPackage ./pkgs/wasgeht { };
           wasgeht-unstable = pkgs.callPackage ./pkgs/wasgeht-unstable { };
@@ -139,233 +194,89 @@
               specialArgs = { inherit inputs nixpkgs; };
             };
         };
-      _images =
-        let
-          all =
-            ({ modulePath, ... }: {
-              imports = [
-                ./modules/nix-common
-                ./modules/ssh-server
-                ./modules/scale-signs
-                ./modules/kube-cluster
-                ./modules/mrtg
-                ./modules/scale-simulator
-                ./modules/wasgeht
-              ];
-            });
-          users =
-            ({ modulePath, ... }: {
-              imports = [
-                ./modules/users
-              ];
-            });
-          nixpkgs = nixos-2511;
-        in
-        {
-          doImage = nixos-2511.lib.nixosSystem {
-            system = "x86_64-linux";
-            modules = [
-              all
-              users
-              ./imgs/do.nix
-            ];
-            specialArgs = { inherit nixpkgs; };
-          };
-          installerImage = nixos-2511.lib.nixosSystem {
-            system = "x86_64-linux";
-            modules = [
-              all
-              ./imgs/gnome-installer.nix
-            ];
-            specialArgs = { inherit nixpkgs; };
-          };
-          piImage =
-            nixos-2511.lib.nixosSystem {
-              system = "aarch64-linux";
-              modules = [
-                all
-                users
-                ./imgs/pi.nix
-              ];
-              specialArgs = { inherit nixpkgs; };
-            };
+      nixosConfigurations = {
+        pi3 = mkSystem {
+          system = "aarch64-linux";
+          modules = [
+            all
+            users
+            ./machines/pis/configuration.nix
+            ./machines/pis/pi3-hardware-configuration.nix
+          ];
+          extraSpecialArgs = { hostname = "pi3"; };
         };
-      nixosConfigurations =
-        let
-          all =
-            ({ modulePath, ... }: {
-              imports = [
-                ./modules/nix-common
-                ./modules/ssh-server
-                ./modules/scale-signs
-                ./modules/kube-cluster
-                ./modules/mrtg
-                ./modules/scale-simulator
-                ./modules/wasgeht
-              ];
-            });
-          users =
-            ({ modulePath, ... }: {
-              imports = [
-                ./modules/users
-              ];
-            });
-          kvm-guest =
-            ({ modulePath, ... }: {
-              imports = [
-                ./modules/kvm-guest
-              ];
-            });
-          soho-router =
-            ({ modulePath, ... }: {
-              imports = [
-                ./modules/dualhome-nat
-                ./modules/dhcp-server
-                ./modules/dns-server
-              ];
-            });
-          nixpkgs = nixos-2511;
-        in
-        {
-          pi3 =
-            let
-              hostname = "pi3";
-            in
-            nixos-2511.lib.nixosSystem {
-              system = "aarch64-linux";
-              modules = [
-                all
-                users
-                ./machines/pis/configuration.nix
-                ./machines/pis/pi3-hardware-configuration.nix
-              ];
-              specialArgs = { inherit nixpkgs hostname inputs; };
-            };
-          pi4 =
-            let
-              hostname = "pi4";
-            in
-            nixos-2511.lib.nixosSystem {
-              system = "aarch64-linux";
-              modules = [
-                all
-                users
-                ./machines/pis/configuration.nix
-                ./machines/pis/pi4-hardware-configuration.nix
-              ];
-              specialArgs = { inherit nixpkgs hostname inputs; };
-            };
-          dev-router = nixos-2511.lib.nixosSystem {
-            system = "x86_64-linux";
-            modules = [
-              all
-              ./machines/dev-router/configuration.nix
-              kvm-guest
-              users
-              soho-router
-            ];
-            specialArgs = { inherit nixpkgs self; };
-          };
-          gibson =
-            nixos-2511.lib.nixosSystem {
-              system = "x86_64-linux";
-              modules = [
-                all
-                ./machines/gibson/configuration.nix
-                users
-              ];
-              specialArgs = { inherit nixpkgs inputs; };
-            };
-          watson = nixos-2511.lib.nixosSystem {
-            system = "x86_64-linux";
-            modules = [
-              ./machines/watson/configuration.nix
-              users
-              all
-            ];
-            specialArgs = { inherit nixpkgs inputs; };
-          };
-          muir = nixos-2511.lib.nixosSystem {
-            system = "x86_64-linux";
-            modules = [
-              nixos-hardware.nixosModules.lenovo-thinkpad-t490
-              ./machines/muir/configuration.nix
-              users
-              all
-            ];
-            specialArgs = { inherit nixpkgs inputs; };
-          };
-          qube =
-            nixos-2511.lib.nixosSystem {
-              system = "x86_64-linux";
-              modules = [
-                ./machines/qube/configuration.nix
-                users
-                all
-              ];
-              specialArgs = { inherit nixpkgs inputs; };
-            };
-          riviera = nixos-2511.lib.nixosSystem {
-            system = "x86_64-linux";
-            modules = [
-              nixos-hardware.nixosModules.lenovo-thinkpad-t490
-              ./machines/riviera/configuration.nix
-            ];
-            specialArgs = { inherit nixpkgs; };
-          };
-          # watson guests
-          k8s-master =
-            let
-              hostname = "k8s-master";
-            in
-            nixos-2511.lib.nixosSystem {
-              system = "x86_64-linux";
-              modules = [
-                all
-                ./machines/watson/guests/kube-api-cluster.nix
-                users
-              ];
-              specialArgs = { inherit nixpkgs hostname inputs; };
-            };
-          k8s-worker1 =
-            let
-              hostname = "k8s-worker1";
-            in
-            nixos-2511.lib.nixosSystem {
-              system = "x86_64-linux";
-              modules = [
-                all
-                ./machines/watson/guests/kube-api-cluster.nix
-                users
-              ];
-              specialArgs = { inherit nixpkgs hostname inputs; };
-            };
-          k8s-worker2 =
-            let
-              hostname = "k8s-worker2";
-            in
-            nixos-2511.lib.nixosSystem {
-              system = "x86_64-linux";
-              modules = [
-                all
-                ./machines/watson/guests/kube-api-cluster.nix
-                users
-              ];
-              specialArgs = { inherit nixpkgs hostname inputs; };
-            };
-          db =
-            let
-              hostname = "db";
-            in
-            nixos-2511.lib.nixosSystem {
-              system = "x86_64-linux";
-              modules = [
-                all
-                ./machines/watson/guests/db.nix
-                users
-              ];
-              specialArgs = { inherit nixpkgs hostname; };
-            };
+        pi4 = mkSystem {
+          system = "aarch64-linux";
+          modules = [
+            all
+            users
+            ./machines/pis/configuration.nix
+            ./machines/pis/pi4-hardware-configuration.nix
+          ];
+          extraSpecialArgs = { hostname = "pi4"; };
         };
+        dev-router = mkSystem {
+          modules = [
+            all
+            users
+            kvm-guest
+            soho-router
+            ./machines/dev-router/configuration.nix
+          ];
+          extraSpecialArgs = { inherit self; };
+        };
+        gibson = mkSystem {
+          modules = [
+            all
+            users
+            ./machines/gibson/configuration.nix
+          ];
+        };
+        watson = mkSystem {
+          modules = [
+            all
+            users
+            ./machines/watson/configuration.nix
+          ];
+        };
+        muir = mkSystem {
+          modules = [
+            all
+            users
+            nixos-hardware.nixosModules.lenovo-thinkpad-t490
+            ./machines/muir/configuration.nix
+          ];
+        };
+        qube = mkSystem {
+          modules = [
+            all
+            users
+            ./machines/qube/configuration.nix
+          ];
+        };
+        riviera = mkSystem {
+          modules = [
+            nixos-hardware.nixosModules.lenovo-thinkpad-t490
+            ./machines/riviera/configuration.nix
+          ];
+        };
+        # watson guests
+        k8s-master = mkSystem {
+          modules = [ all users ./machines/watson/guests/kube-api-cluster.nix ];
+          extraSpecialArgs = { hostname = "k8s-master"; };
+        };
+        k8s-worker1 = mkSystem {
+          modules = [ all users ./machines/watson/guests/kube-api-cluster.nix ];
+          extraSpecialArgs = { hostname = "k8s-worker1"; };
+        };
+        k8s-worker2 = mkSystem {
+          modules = [ all users ./machines/watson/guests/kube-api-cluster.nix ];
+          extraSpecialArgs = { hostname = "k8s-worker2"; };
+        };
+        db = mkSystem {
+          modules = [ all users ./machines/watson/guests/db.nix ];
+          extraSpecialArgs = { hostname = "db"; };
+        };
+      };
     };
 }
