f75bebce77d1e2533b12e31b83c39ff83b783d08
kylerisse <kylerisse@users.noreply.github.com>
2026-04-13 19:38:46 -0700

    mod.mimir: init

---
+++ modules/mimir/default.nix
@@ -0,0 +1,63 @@
+{ config
+, lib
+, ...
+}:
+let
+  cfg = config.mynixcfg.mimir;
+in
+{
+  options.mynixcfg.mimir = {
+    enable = lib.mkEnableOption "Mimir time series database";
+
+    httpListenPort = lib.mkOption {
+      type = lib.types.int;
+      default = 3200;
+    };
+
+    grpcListenPort = lib.mkOption {
+      type = lib.types.int;
+      default = 9096;
+    };
+
+    dataDir = lib.mkOption {
+      type = lib.types.str;
+      default = "/var/lib/mimir";
+    };
+  };
+
+  config = lib.mkIf cfg.enable {
+    systemd.services.mimir = {
+      after = [ "network-online.target" ];
+      wants = [ "network-online.target" ];
+    };
+
+    services.mimir = {
+      enable = true;
+      configuration = {
+        multitenancy_enabled = false;
+        server = {
+          http_listen_port = cfg.httpListenPort;
+          grpc_listen_port = cfg.grpcListenPort;
+        };
+        common.storage = {
+          backend = "filesystem";
+          filesystem.dir = "${cfg.dataDir}/data";
+        };
+        blocks_storage = {
+          storage_prefix = "blocks";
+          tsdb.dir = "${cfg.dataDir}/tsdb";
+        };
+        compactor = {
+          data_dir = "${cfg.dataDir}/compactor";
+          compaction_interval = "30m";
+        };
+        ingester.ring = {
+          replication_factor = 1;
+          kvstore.store = "inmemory";
+        };
+      };
+    };
+
+    networking.firewall.allowedTCPPorts = [ cfg.httpListenPort ];
+  };
+}
