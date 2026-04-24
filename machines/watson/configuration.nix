2634e2826f47e1c680fcabc9ba1cb1314845b713
kylerisse <kylerisse@users.noreply.github.com>
2026-04-24 14:39:01 -0700

    watson: fix pcie interface rename

--- machines/watson/configuration.nix
+++ machines/watson/configuration.nix
@@ -49,8 +49,8 @@ in
       Name = "br0";
     };
     networks = {
-      "20-enp8s0" = {
-        matchConfig.Name = "enp8s0";
+      "20-enp5s0" = {
+        matchConfig.Name = "enp5s0";
         networkConfig = {
           Bridge = "br0";
           DHCP = "no";
